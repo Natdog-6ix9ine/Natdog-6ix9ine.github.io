629dc368268f52b75f23a1561831b182d9d334b3
Natdog-6ix9ine <Natdog-6ix9ine@users.noreply.github.com>
2024-12-09 16:09:29 +0000

    deploy: Natdog-6ix9ine/natdog@b92e36ac1483e04e0ab8e96d85ff1a9be7f630e6

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/09/hello-world/"];function toRandomPost(){
+var posts=["2024/12/09/hello-world/","2024/12/09/创建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
