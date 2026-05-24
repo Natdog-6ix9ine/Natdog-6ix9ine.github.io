7715185d5d62a2bedc340b72988a00b5d16807d4
Natdog-6ix9ine <Natdog-6ix9ine@users.noreply.github.com>
2026-05-24 07:32:28 +0000

    deploy: Natdog-6ix9ine/natdog@e61ac0481cf517fadc2fcc65f46d9aca906e910d

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/05/24/创建算子/","2026/05/24/测试自动部署/","2026/05/24/新环境写博客/","2026/05/24/配置环境/"];function toRandomPost(){
+var posts=["2026/05/24/创建算子/","2026/05/24/新环境写博客/","2026/05/24/测试自动部署/","2026/05/24/配置环境/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
